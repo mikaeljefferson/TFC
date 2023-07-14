03807a1029ef8994c1c8ba94ac5e7f55a823ac7e
mikaeljefferson <mikaeljeffeson@gmail.com>
2023-07-14 10:47:41 -0300

    Feat: Req 01

--- app/backend/src/database/models/team.model.ts
+++ app/backend/src/database/models/team.model.ts
@@ -1,34 +1,34 @@
 import {
-    DataTypes,
-    Model,
-    InferAttributes,
-    InferCreationAttributes,
-    CreationOptional,
-  } from 'sequelize';
-  import db from '.';
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
   
-  class Team extends Model<InferAttributes<Team>,
-  InferCreationAttributes<Team>> {
-    declare id: CreationOptional<number>;
-    declare teamName: string;
-  }
-  
-  Team.init({
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    teamName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+class Team extends Model<InferAttributes<Team>,
+InferCreationAttributes<Team>> {
+  declare id: CreationOptional<number>;
+  declare teamName: string;
+}
+
+Team.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
   }, {
-    sequelize: db,
-    modelName: 'teams',
-    timestamps: false,
-    underscored: true,
-  });
-  
-  export default Team;
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+  underscored: true,
+});
+
+export default Team;
